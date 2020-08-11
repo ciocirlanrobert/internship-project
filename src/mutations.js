e4ce83608e97191eccba811777d97519ca547c55
Ciocirlan Robert <robert.ciocirlan@moduscreate.com>
2020-08-11 14:16:16 +0300

    Add mutation for updating contactinfo

--- src/mutations.js
+++ src/mutations.js
@@ -1,59 +1,59 @@
 import { gql } from "@apollo/client";
 
 export const UpdateContactInfo = gql`
-	mutation UpdateContactInfo(
-		$id: Int!
-		$email: String
-		$phone: String
-		$city: String
-		$website: String
-		$avatarUrl: String
-		$about: String
-		$countryId: Int
-	) {
-		updateContactInfo(
-			id: $id
-			email: $email
-			phone: $phone
-			city: $city
-			website: $website
-			avatarUrl: $avatarUrl
-			about: $about
-			countryId: $countryId
-		) {
-			id
-		}
-	}
+  mutation UpdateContactInfo(
+    $id: Int!
+    $email: String
+    $phone: String
+    $city: String
+    $website: String
+    $avatarUrl: String
+    $about: String
+    $countryId: Int
+  ) {
+    updateContactInfo(
+      id: $id
+      email: $email
+      phone: $phone
+      city: $city
+      website: $website
+      avatarUrl: $avatarUrl
+      about: $about
+      countryId: $countryId
+    ) {
+      id
+    }
+  }
 `;
 
 export const UpdateUserContactInfo = gql`
-	mutation UpdateUserContactInfo($contactInfoId: Int!, $id: Int!) {
-		updateUser(id: $id, contactInfoId: $contactInfoId) {
-			id
-		}
-	}
+  mutation UpdateUserContactInfo($contactInfoId: Int!, $id: Int!) {
+    updateUser(id: $id, contactInfoId: $contactInfoId) {
+      id
+    }
+  }
 `;
 
 export const CreateContactInfo = gql`
-	mutation createContactInfo(
-		$email: String!
-		$phone: String!
-		$city: String!
-		$website: String!
-		$avatarUrl: String!
-		$about: String!
-		$countryId: Int!
-	) {
-		createContactInfo(
-			email: $email
-			phone: $phone
-			city: $city
-			website: $website
-			avatarUrl: $avatarUrl
-			about: $about
-			countryId: $countryId
-		) {
-			id
-		}
-	}
+  mutation createContactInfo(
+    $email: String!
+    $phone: String!
+    $city: String!
+    $website: String!
+    $avatarUrl: String!
+    $about: String!
+    $countryId: Int!
+  ) {
+    createContactInfo(
+      email: $email
+      phone: $phone
+      city: $city
+      website: $website
+      avatarUrl: $avatarUrl
+      about: $about
+      countryId: $countryId
+    ) {
+      id
+    }
+  }
 `;
