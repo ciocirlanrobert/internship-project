81ebaf309ee98316c203b6d84f361cda2c5bfdc6
Ciocirlan Robert <ciocirlan.r@mail.com>
2020-11-09 20:12:44 +0200

    Restyled Landing's Page header

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,104 +1,104 @@
-import React from "react";
+import React from 'react'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import Login from "./pages/LoginRegister/Login";
-import Register from "./pages/LoginRegister/Register";
-import WelcomePage from "./pages/WelcomePage/WelcomePage";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import { useUserContext } from "./context/UserContext";
-import MyProfile from "./pages/MyProfile";
-import Admin from "./pages/Admin Dashboard/Admin";
-import Company from "./pages/Company Dashboard/Company";
-import JobDetail from "./pages/JobDetail/JobDetail";
-import UserJobApplications from "./pages/UserJobApplications/UserJobApplications";
-import CompanyJobApplications from "./pages/CompanyJobApplications/CompanyJobApplications";
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom'
+import Login from './pages/LoginRegister/Login'
+import Register from './pages/LoginRegister/Register'
+import WelcomePage from './pages/WelcomePage/WelcomePage'
+import LandingPage from './pages/LandingPage/LandingPage'
+import { useUserContext } from './context/UserContext'
+import MyProfile from './pages/MyProfile'
+import Admin from './pages/Admin Dashboard/Admin'
+import Company from './pages/Company Dashboard/Company'
+import JobDetail from './pages/JobDetail/JobDetail'
+import UserJobApplications from './pages/UserJobApplications/UserJobApplications'
+import CompanyJobApplications from './pages/CompanyJobApplications/CompanyJobApplications'
 
 export default function RouteHandler() {
-  const { user } = useUserContext();
+    const { user } = useUserContext()
 
-  const commonRoutes = [
-    {
-      path: "/myProfile",
-      component: MyProfile,
-    },
-  ];
+    const commonRoutes = [
+        {
+            path: '/myProfile',
+            component: MyProfile,
+        },
+    ]
 
-  const adminRoutes = [
-    {
-      path: "/admin",
-      component: Admin,
-    },
-  ];
+    const adminRoutes = [
+        {
+            path: '/admin',
+            component: Admin,
+        },
+    ]
 
-  const simpleUserRoutes = [
-    {
-      path: "/applications",
-      component: UserJobApplications,
-    },
-  ];
+    const simpleUserRoutes = [
+        {
+            path: '/applications',
+            component: UserJobApplications,
+        },
+    ]
 
-  const companyUserRoutes = [
-    {
-      path: "/company",
-      component: Company,
-    },
-    {
-      path: "/applicants",
-      component: CompanyJobApplications,
-    },
-  ];
+    const companyUserRoutes = [
+        {
+            path: '/company',
+            component: Company,
+        },
+        {
+            path: '/applicants',
+            component: CompanyJobApplications,
+        },
+    ]
 
-  return (
-    <Router>
-      <Switch>
-        {/* <Route exact path="/landingPage" component={LandingPage} /> */}
-        <Route exact path="/login" component={Login} />
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                {/* <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/" component={WelcomePage} />
-        <Route path="/jobs/:id" component={JobDetail} />
-        {user.userRoleId > 0 &&
-          commonRoutes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              exact
-            />
-          ))}
-        {user.userRoleId === 3 &&
-          adminRoutes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              exact
-            />
-          ))}
-        {user.userRoleId === 2 &&
-          companyUserRoutes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              exact
-            />
-          ))}
-        {user.userRoleId === 1 &&
-          simpleUserRoutes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              component={route.component}
-              exact
-            />
-          ))}
-        ;
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
+        <Route exact path="/" component={WelcomePage} /> */}
+                <Route path="/jobs/:id" component={JobDetail} />
+                {user.userRoleId > 0 &&
+                    commonRoutes.map((route) => (
+                        <Route
+                            key={route.path}
+                            path={route.path}
+                            component={route.component}
+                            exact
+                        />
+                    ))}
+                {user.userRoleId === 3 &&
+                    adminRoutes.map((route) => (
+                        <Route
+                            key={route.path}
+                            path={route.path}
+                            component={route.component}
+                            exact
+                        />
+                    ))}
+                {user.userRoleId === 2 &&
+                    companyUserRoutes.map((route) => (
+                        <Route
+                            key={route.path}
+                            path={route.path}
+                            component={route.component}
+                            exact
+                        />
+                    ))}
+                {user.userRoleId === 1 &&
+                    simpleUserRoutes.map((route) => (
+                        <Route
+                            key={route.path}
+                            path={route.path}
+                            component={route.component}
+                            exact
+                        />
+                    ))}
+                ;
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    )
 }
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,79 +1,36 @@
-import React from "react";
-import Navbar from "../../Components/Navbar/Navbar";
-import Footer from "../../Components/Footer";
-import { makeStyles } from "@material-ui/core/styles";
-import { useHistory } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import JobsContainer from "../../Components/Jobs";
-
-const useStyles = makeStyles((theme) => ({
-  heroContent: {
-    backgroundColor: "black",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    padding: theme.spacing(8, 0, 6),
-    color: "white",
-    height: "90vh",
-  },
-  heroContainer: {
-    display: "flex",
-    flexDirection: "column",
-    color: "white",
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  jobsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "40px 20px",
-    height: "100vh",
-  },
-}));
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import Banner from '../../images/landingBanner.svg'
+import './style/style.css'
 
 export default function LandingPage() {
-  const style = useStyles();
-
-  return (
-    <>
-      <Navbar />
-      <div className={style.heroContent}>
-        <Container maxWidth="sm" className={style.heroContainer}>
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="inherit"
-            gutterBottom
-          >
-            Lorem Ipsum
-          </Typography>
-          <Typography variant="h5" align="center" color="inherit" paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-            tincidunt lorem leo, et rhoncus massa aliquam vel. Quisque a metus
-            interdum, vulputate ligula ut, gravida velit. Aenean elit metus,
-            tempus sed tortor a, condimentum congue felis.
-          </Typography>
-          <div className={style.heroButtons}>
-            <Grid container spacing={2} justify="center">
-              <Grid item>
-                <Button variant="contained" color="primary">
-                  See jobs!
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
+    return (
+        <Container className="root" maxWidth="lg">
+            <Navbar />
+            <div className="container">
+                <div className="leftContainer">
+                    <h1 className="title">
+                        Join Our Team At <span className="company">Roj</span>
+                    </h1>
+                    <p className="description">
+                        Work at the most dynamic and successful agency
+                    </p>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        data-path="/login"
+                        size="large"
+                        style={{ backgroundColor: '#EF6C35' }}
+                    >
+                        View Openings
+                    </Button>
+                </div>
+                <div className="rightContainer">
+                    <img src={Banner} alt="Banner" />
+                </div>
+            </div>
         </Container>
-      </div>
-      <div className={style.jobsContainer}>
-        <h1>Open jobs</h1>
-        <JobsContainer />
-      </div>
-      <Footer />
-    </>
-  );
+    )
 }
