6f64eea3c202b45503a5466f0badecb52ce8ae62
Ciocirlan Robert <ciocirlan.r@mail.com>
2020-11-17 21:39:13 +0200

    Styled job card

--- src/pages/LandingPage/JobCard.jsx
+++ src/pages/LandingPage/JobCard.jsx
@@ -10,10 +10,20 @@ export default function JobCard(props) {
                 <span className="jobCompany">{props.company.name}</span> |
                 Full-time or Contract
             </p>
+            {/* Temporarily inline styling */}
             <Button
+                size="large"
                 variant="contained"
                 color="primary"
                 className="jobCardButton"
+                style={{
+                    backgroundColor: '#FFF3EC',
+                    color: ' #ef6c35',
+                    padding: '15px 30px',
+                    borderRadius: '30px',
+                    boxShadow: 'none',
+                    textTransform: 'capitalize',
+                }}
             >
                 Apply now
             </Button>
