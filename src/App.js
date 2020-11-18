3ac8314ae91d597d54789467e4c568e50d1a767e
Ciocirlan Robert <ciocirlan.r@mail.com>
2020-11-18 02:03:46 +0200

    Format indenting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import RouteHandler from "./Router.jsx";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import UserContextProvider from "./context/UserContext";
-import ErrorBoundary from "./utils/ErrorBoundary";
-import {ToastProvider} from "react-toast-notifications";
+import React from 'react'
+import RouteHandler from './Router.jsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import UserContextProvider from './context/UserContext'
+import ErrorBoundary from './utils/ErrorBoundary'
+import { ToastProvider } from 'react-toast-notifications'
 
 const client = new ApolloClient({
-  uri: "http://localhost:4001/gql",
-  cache: new InMemoryCache(),
-  onError: ({ networkError, graphQLErrors }) => {
-    console.log("graphQLErrors", graphQLErrors);
-    console.log("networkError", networkError);
-  },
-});
+    uri: 'http://localhost:4001/gql',
+    cache: new InMemoryCache(),
+    onError: ({ networkError, graphQLErrors }) => {
+        console.log('graphQLErrors', graphQLErrors)
+        console.log('networkError', networkError)
+    },
+})
 
 export default function App() {
     return (
-    <ApolloProvider client={client}>
-      <ToastProvider>
-      <UserContextProvider>
-      <ErrorBoundary>
-        <RouteHandler />
-        </ErrorBoundary>
-      </UserContextProvider>
-      </ToastProvider>
-    </ApolloProvider>
-  );
+        <ApolloProvider client={client}>
+            <ToastProvider>
+                <UserContextProvider>
+                    <ErrorBoundary>
+                        <RouteHandler />
+                    </ErrorBoundary>
+                </UserContextProvider>
+            </ToastProvider>
+        </ApolloProvider>
+    )
 }
--- src/utils/ErrorBoundary.js
+++ src/utils/ErrorBoundary.js
@@ -1,31 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 export default class ErrorBoundary extends Component {
     constructor(props) {
-        super(props);
-        this.state = {hasError: false};
+        super(props)
+        this.state = { hasError: false }
     }
 
     static getDerivedStateFromError(error) {
-        return {hasError: true};
+        return { hasError: true }
     }
 
     componentDidCatch(error, errorInfo) {
-        console.error("ErrorBoundary caught an error", error, errorInfo);
+        console.error('ErrorBoundary caught an error', error, errorInfo)
     }
-
 
     render() {
-
-        if(this.state.hasError) {
-            return (
-                <h1>
-                    There was an error. Go back to the home page.
-                </h1>
-            )
+        if (this.state.hasError) {
+            return <h1>There was an error. Go back to the home page.</h1>
         }
 
-        return this.props.children;
+        return this.props.children
     }
-
 }
